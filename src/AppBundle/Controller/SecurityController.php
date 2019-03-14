4f4c5a75fde6ec0473d3086e09935f67610a9ce1
totel <totel.dbmdr@gmail.com>
2019-03-14 17:33:37 +0100

    seguridad

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,22 @@
+<?php 
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    public function loginAction(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+}
